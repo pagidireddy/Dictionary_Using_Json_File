b488bc41e0d88ffb65778241332a305c1413d77a
pagidireddy <prashanthpagidireddy@gmail.com>
2024-02-19 11:13:13 +0530

    Initial commit: JSON data and Python code

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import json
+from difflib import get_close_matches
+
+file = json.load(open("data/dictionary.json"))
+
+
+def data(word):
+    if word.lower() in file:
+        return file[word.lower()]
+    if word.title() in file:
+        return file[word.title()]
+    elif word.upper() in file:
+        return file[word.upper()]
+    elif len(get_close_matches(word, file.keys())) > 0:
+        x = input("r u looking for '%s' enter Y if yes else N: " % get_close_matches(word, file.keys())[0])
+        if x == "Y" or x == 'y':
+            return file[get_close_matches(word, file.keys())[0]]
+        if x == 'N' or x == 'n':
+            return "check ur word and enter again"
+        else:
+            return "invalid input"
+    else:
+        return "The word not found"
